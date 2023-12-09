e456dbf156a020c9699db33cb405246b0811e897
paola1611 <dp673162@gmail.com>
2023-12-09 16:52:05 -0400

    proyecto inicial ionic

---
+++ agente-de-transito/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agente-de-transito',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
