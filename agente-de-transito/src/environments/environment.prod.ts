5eb16f344c1f2a9b8b8a78651518631a11e0586d
paola1611 <dp673162@gmail.com>
2023-12-11 21:22:25 -0400

    subiendo apk

--- agente-de-transito/src/environments/environment.prod.ts
+++ agente-de-transito/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC1Lay12PMKyY7cqgv8iKjXIb-7l0zvixg",
+    authDomain: "agente-de-transito-76b2f.firebaseapp.com",
+    projectId: "agente-de-transito-76b2f",
+    storageBucket: "agente-de-transito-76b2f.appspot.com",
+    messagingSenderId: "786622370892",
+    appId: "1:786622370892:web:18297aabc3993135cf50e9",
+    measurementId: "G-6W8WLM9SH6"
+  }
 };
